6e5327e6649a21030d95b6984a0ce757fa7c17ec
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-04-01 16:35:40 +0300

    Add files via upload

---
+++ task50.cs
@@ -0,0 +1,37 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 10);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.Write("Введите строку элемента: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец элемента:");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+if (pos1 < 0 || pos1 > matrix.GetLength(0) | pos2 < 0 | pos2 > matrix.GetLength(1) - 1)
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента: {matrix[pos1, pos2]}");
+}
